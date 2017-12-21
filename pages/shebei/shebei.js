4fef074991c3f76514e140656773c36b9426b34c
yitian1808 <yitian1808@163.com>
2017-12-21 17:02:45 +0800

    sss

--- pages/shebei/shebei.js
+++ pages/shebei/shebei.js
@@ -1,69 +1,79 @@
-// pages/jieshao/jieshao.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
-  
+    tabArr: {
+      curHdIndex: 0,
+      curBdIndex: 0
+    },
   },
+  tabFun: function (e) {
+    //获取触发事件组件的dataset属性 
+    var _datasetId = e.target.dataset.id;
+    console.log("----" + _datasetId + "----");
+    var _obj = {};
+    _obj.curHdIndex = _datasetId;
+    _obj.curBdIndex = _datasetId;
+    this.setData({
+      tabArr: _obj
+    });
+  }, 
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    wx.showNavigationBarLoading();
-    wx.setNavigationBarTitle({
-      title: '项目介绍'
-    })
+    
   },
+
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+    
   }
 })
